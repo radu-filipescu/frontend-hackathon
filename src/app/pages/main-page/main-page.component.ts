f0bb5e15f504a6be867245f12b5afd35b4a893ec
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-04-01 22:12:41 +0300

    icons

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faBrain, faSeedling, faBicycle,  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-page',
@@ -7,6 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+
+  faBrain = faBrain;
 
   showDescriptions: boolean[] = [false, false, false, false, false];
 
