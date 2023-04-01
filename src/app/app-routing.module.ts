3d9686c74af48c035d8cec6d87f13af5f9561280
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-04-01 19:05:09 +0300

    leaderboards2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { QuizPageComponent } from './pages/quiz-page/quiz-page.component';
 import { ProofPageComponent } from './pages/proof-page/proof-page.component';
+import { InternalLeaderboardPageComponent } from './pages/internal-leaderboard-page/internal-leaderboard-page.component';
+import { ExternalLeaderboardPageComponent } from './pages/external-leaderboard-page/external-leaderboard-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'quiz', component: QuizPageComponent },
-  { path: 'proof', component: ProofPageComponent}
+  { path: 'internal', component: InternalLeaderboardPageComponent },
+  { path: 'external', component: ExternalLeaderboardPageComponent },
+  { path: 'quiz', component: QuizPageComponent },
+  { path: 'proof/:action', component: ProofPageComponent}
 ];
 
 @NgModule({
