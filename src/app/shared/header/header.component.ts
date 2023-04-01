c71b1d547cc76d971dbb4bbe17501d4fd2c07ddc
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-04-01 20:40:30 +0300

    push

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  faSignOutAlt = faSignOutAlt;
 
   constructor(private router: Router) { }
 
