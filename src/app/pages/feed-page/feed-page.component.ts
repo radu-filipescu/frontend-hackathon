d509994e73073dac70ce789c86cdd2a4b947e96a
Sebastian Gradinaru <gradinaru.sebastian1@gmail.com>
2023-04-02 03:50:25 +0300

    feed images + sticky header (gata cu actiunile)

--- src/app/pages/feed-page/feed-page.component.ts
+++ src/app/pages/feed-page/feed-page.component.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UserDTO } from 'src/app/DTOs/UserDTO';
+import { ImageDTO } from 'src/app/DTOs/ImageDTO';
 import { HistoryDTO } from 'src/app/DTOs/historyDTO';
 import { HistoryService } from 'src/app/service/history.service';
 import { CONFIG } from 'src/app/shared/CONFIG';
 import { Action } from 'src/app/shared/Action';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-feed-page',
@@ -15,9 +17,13 @@ export class FeedPageComponent implements OnInit {
   CONFIG: CONFIG = new CONFIG();
   posts: HistoryDTO[] = [];
   usersMap: Map<number, string> = new Map<number, string>();
+  photoMap: Map<string, SafeResourceUrl> = new Map<string, string>();
   actionTranslator: Action = new Action();
 
-  constructor(private historyService : HistoryService, private httpClient: HttpClient) { }
+  constructor(
+    private historyService : HistoryService, 
+    private httpClient: HttpClient,
+    private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.refreshFeed();
@@ -36,6 +42,11 @@ export class FeedPageComponent implements OnInit {
         .subscribe(res => {
           for(let post of res){
             this.posts.push(post);
+
+            this.httpClient.get<ImageDTO>(this.CONFIG.backendDevAPI + 'Image/' + post.photoPath)
+              .subscribe(image => {
+                  this.photoMap.set(image.imageName, this._sanitizer.bypassSecurityTrustResourceUrl(image.imageAsBase64));
+              })
           }
         }
       )
